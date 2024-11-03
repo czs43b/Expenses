8113dbd637883505c4354fc8a1d4ce5c2d0189ca
czs43b <34371393+czs43b@users.noreply.github.com>
2024-11-03 22:12:48 +0000

    Lazy load components

---
+++ .firebase/expenses-5d437/hosting/chunk-DYIMXQBJ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-W73NIBRV.js";import"./chunk-5U7AXR3O.js";import"./chunk-PTYEF7PQ.js";import"./chunk-6H46YYVM.js";export{a as LoginComponent};
--- .firebase/expenses-5d437/hosting/chunk-VYCNKWEF.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YNDMNXWM.js";import"./chunk-GOILVWMB.js";import"./chunk-6H46YYVM.js";export{a as LoginComponent};
